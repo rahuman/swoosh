194724a4027ed26f0a1cd5c3b76b445f231d8742
Fazlul Fazal <rahuman@gmail.com>
2019-04-04 15:52:46 +1100

    Lesson-04 040419

--- app/src/main/java/com/itguys4life/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/itguys4life/swoosh/LeagueActivity.kt
@@ -3,7 +3,7 @@ package com.itguys4life.swoosh
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/itguys4life/swoosh/MainActivity.kt
+++ app/src/main/java/com/itguys4life/swoosh/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.itguys4life.swoosh
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_welcome.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
