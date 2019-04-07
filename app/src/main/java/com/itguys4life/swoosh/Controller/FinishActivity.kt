992e02a8965188965f16130d59a2fb2149713405
Fazlul Fazal <rahuman@gmail.com>
2019-04-07 14:50:01 +1000

    Lesson-08

--- app/src/main/java/com/itguys4life/swoosh/Controller/FinishActivity.kt
+++ app/src/main/java/com/itguys4life/swoosh/Controller/FinishActivity.kt
@@ -3,11 +3,19 @@ package com.itguys4life.swoosh.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.itguys4life.swoosh.R
+import com.itguys4life.swoosh.Utilities.EXTRA_LEAGUE
+import com.itguys4life.swoosh.Utilities.EXTRA_SKILL
+import kotlinx.android.synthetic.main.activity_finish.*
 
 class FinishActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
+
+        val league = intent.getStringExtra(EXTRA_LEAGUE)
+        val skill = intent.getStringExtra(EXTRA_SKILL)
+
+        searchLeagueText.text = "Looking for $league $skill league near you..."
     }
 }
--- app/src/main/java/com/itguys4life/swoosh/Utilities/Constants.kt
+++ app/src/main/java/com/itguys4life/swoosh/Utilities/Constants.kt
@@ -1,3 +1,4 @@
 package com.itguys4life.swoosh.Utilities
 
 const val EXTRA_LEAGUE = "league"
+const val EXTRA_SKILL = "skill"
