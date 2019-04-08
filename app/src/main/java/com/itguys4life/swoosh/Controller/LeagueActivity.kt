395320a7c4fb6e61cafe0c4c1e45a1b136347394
Fazlul Fazal <rahuman@gmail.com>
2019-04-08 15:52:19 +1000

    Lesson-10 Added New Icons

--- app/src/main/java/com/itguys4life/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/itguys4life/swoosh/Controller/LeagueActivity.kt
@@ -3,6 +3,7 @@ package com.itguys4life.swoosh.Controller
 import android.content.Intent
 import android.os.Bundle
 import android.os.Parcelable
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.itguys4life.swoosh.Model.Player
@@ -14,9 +15,22 @@ class LeagueActivity : BaseActivity() {
 
     var player = Player("", "")
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+        if (savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+
     }
 
     fun onMensClicked(view: View) {
